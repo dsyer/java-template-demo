2c0d34d95a6a6db73c00ea42b2e0580b05a58589
Dave Syer <dsyer@vmware.com>
2024-03-22 12:30:12 +0000

    Add @Configuration annotation

--- src/main/java/demo/RockerAutoConfiguration.java
+++ src/main/java/demo/RockerAutoConfiguration.java
@@ -1,8 +1,10 @@
 package demo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 
+@Configuration
 public class RockerAutoConfiguration {
 	
 	@Bean
