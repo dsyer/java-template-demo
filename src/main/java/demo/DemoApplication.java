82a731d79df8dc4f598d194d328995f859c2ca5b
Dave Syer <dsyer@vmware.com>
2024-03-22 14:06:41 +0000

    Update and simplify

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,19 +1,12 @@
 package demo;
 
-import java.time.LocalDate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.jstach.jstache.JStacheFlags;
-import io.jstach.jstache.JStacheFlags.Flag;
-import io.jstach.jstache.JStacheFormatterTypes;
 import io.jstach.jstache.JStachePath;
 
 @SpringBootApplication
 @JStachePath(prefix = "templates/", suffix = ".mustache")
-@JStacheFlags(flags = Flag.DEBUG)
-@JStacheFormatterTypes(types = LocalDate.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
